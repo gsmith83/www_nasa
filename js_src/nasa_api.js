8d75dc7c6d6a40cfd63043572010cd45f329dcfd
Gregory Smith <gsmith83@msn.com>
2016-08-20 12:16:15 -0600

    Updated css, added nasa logo.png

--- js_src/nasa_api.js
+++ js_src/nasa_api.js
@@ -6,6 +6,7 @@ if (month < 10)
 else
     todayDateISO = todayDate.getFullYear() + "-" + month + "-" + todayDate.getDate();
 
+// calls other functions to display nasa daily digest
 function displayNASAStuff() {
     var apiKey = "0b33tVGBkzuCWCTpuQCyyF2NhDxbVRu7kcsN9snr";
 
@@ -14,6 +15,7 @@ function displayNASAStuff() {
     httpGetAsync('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=' + apiKey, displayMarsRover)
 }
 
+// Async http getter
 function httpGetAsync(url, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
@@ -25,6 +27,17 @@ function httpGetAsync(url, callback) {
 }
 
 // Gets the apod and info and finds the html tags to display them in
+/*
+copyright:"El Cielo de Canarias"
+date:"2016-08-20"
+explanation:"Gamma-rays and dust from periodic Comet Swift-Tuttle plowed through planet Earth's atmosphere on the night of August 11/12. Impacting at about 60 kilometers per second the grains of comet dust produced this year's remarkably active Perseid meteor shower. This composite wide-angle image of aligned shower meteors covers a 4.5 hour period on that Perseid night. In it the flashing meteor streaks can be traced back to the shower's origin on the sky. Alongside the Milky Way in the constellation Perseus, the radiant marks the direction along the perodic comet's orbit. Traveling at the speed of light, cosmic gamma-rays impacting Earth's atmosphere generated showers too, showers of high energy particles. Just as the meteor streaks point back to their origin, the even briefer flashes of light from the particles can be used to reconstruct the direction of the particle shower, to point back to the origin on the sky of the incoming gamma-ray. Unlike the meteors, the incredibly fast particle shower flashes can't be followed by eye. But both can be followed by the high speed cameras on the multi-mirrored dishes in the foreground. Of course, the dishes are MAGIC (Major Atmospheric Gamma Imaging Cherenkov) telescopes, an Earth-based gamma-ray observatory on the Canary Island of La Palma."
+hdurl:"http://apod.nasa.gov/apod/image/1608/PerseidsMAGIC_DLopez.jpg"
+media_type:"image"
+service_version:"v1"
+title:"Gamma-rays and Comet Dust"
+url:"http://apod.nasa.gov/apod/image/1608/PerseidsMAGIC_DLopez1024.jpg"
+*/
+
 function displayAPOD(xml) {
     var responseArr = JSON.parse(xml.responseText);
     if (responseArr != null) {
@@ -36,6 +49,9 @@ function displayAPOD(xml) {
 }
 
 // Finds all neos on today's date and displays them by generating html tags
+/* Response Array
+
+*/
 function displayNEOFeed(xml) {
     var responseArr = JSON.parse(xml.responseText);
     var elementCount = responseArr["element_count"];
